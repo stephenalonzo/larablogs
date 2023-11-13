9d03a7b4b318a933652978881f550a1b4b818e92
Stephen Alonzo <stephena0228@gmail.com>
2023-11-13 11:47:02 -0800

    Rearrange layout of blog posts on homepage

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -2,22 +2,21 @@
 
 @section('content')
 <section class="px-4 py-6">
-    <div class="container mx-auto space-y-4 flex flex-col items-center justify-center">
+    <div class="container mx-auto space-y-8 flex flex-col items-center justify-center">
         <div class="space-y-2 text-center">
             <h3 class="text-sm font-semibold text-purple-700">Larablogs</h3>
-            <h1 class="text-3xl font-semibold">The latest industry news, interviews, technologies, and resources.</h1>
+            <h1 class="text-4xl font-semibold">The latest writings from our team</h1>
+            <p class="text-gray-400">The latest industry news, interviews, technologies, and resources.</p>
         </div>
         <form action="/?search=" method="GET">
-            <div class="flex items-center">
-                <input type="text" name="search" id="" class="px-4 py-2 rounded-l-md rounded-tl-md rounded-bl-md border border-gray-300"
+            <div class="relative">
+                <input type="text" name="search" id="" class="pl-9 pr-4 py-2 rounded-md border border-gray-300"
                 @if (isset($_GET['search']))
                     value="{{ $_GET['search'] }}"
                 @else
                     placeholder="Search"
                 @endif>
-                <button type="submit" class="px-4 py-2 rounded-r-md rounded-tr-md rounded-br-md bg-purple-700 border border-purple-700 text-white font-semibold">
-                    <i class="fas fa-search"></i>
-                </button>
+                <i class="fas fa-search absolute top-1/2 transform -translate-y-1/2 left-0 text-gray-400 pl-3"></i>
             </div>
         </form>
     </div>
@@ -25,10 +24,16 @@
 <hr class="container mx-auto">
 <section class="px-4 py-6">
     <div class="container mx-auto">
-        <div class="grid grid-cols-12 gap-6">
+        <div class="grid grid-cols-12 gap-8">
             @foreach ($posts as $post)
-            <div class="col-span-4 gap-6 flex flex-col items-start justify-between">
-                <img src="{{ asset('storage/' . $post->image) }}" alt="" class="w-full h-full rounded-md">
+            <div class="col-span-4 gap-4 flex flex-col items-start justify-between">
+                <img src="{{ asset('storage/' . $post->image) }}" alt="" class="w-full h-48 rounded-md object-cover">
+                <div class="p-2 rounded-full bg-purple-200 text-xs space-x-1 text-purple-700">
+                    <a href="/?category={{ $post->category }}" class="px-2 py-1 rounded-full bg-white text-purple-700">
+                        {{ $post->category }}
+                    </a>
+                    <span class="text-xs">{{ $post->mins_to_read }} min read</span>
+                </div>
                 <div class="flex flex-col items-start justify-between space-y-4 w-full">
                     <div class="flex flex-row items-center space-x-4">
                         <h5 class="font-semibold text-purple-700 text-base">{{ $post->user->name }}</h5>
@@ -41,17 +46,17 @@
                             </a>
                             <a href="{{ route('blog.show', $post->id) }}"><i class="fas fa-long-arrow-alt-right"></i></a>
                         </div>
-                        <p class="text-gray-500">
+                        <p class="text-gray-500 truncate">
                             {{ strip_tags($post->description) }}
                         </p>
                     </div>
-                    <a href="/?category={{ $post->category }}" class="px-3 py-1 rounded-full bg-purple-200 text-xs text-purple-700">
-                        {{ $post->category }}
-                    </a>
                 </div>
             </div>
             @endforeach
         </div>
     </div>
 </section>
+<div class="px-4 py-6">
+    {{ $posts->links() }}
+</div>
 @endsection
